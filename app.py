33569adba4ae72de5949ff7bd0130fe61e9d333b
christopherBuba <77661772+christopherBuba@users.noreply.github.com>
2023-02-21 10:16:10 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+import firebase_admin
+from firebase_admin import credentials, storage
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+# Initialize Firebase app and credentials
+cred = credentials.Certificate('./elizafashions-1679-firebase-adminsdk-hhr9d-0eeb6fe5b7.json')
+firebase_admin.initialize_app(cred, {
+    'storageBucket': 'elizafashions-1679.appspot.com'
+})
+bucket = storage.bucket()
+
+
+# Route that fetches image and reads hidden code
+@app.route('/')
+def get_hidden_code():
+    blob = bucket.blob('sisyphus.jpeg')
+    content = blob.download_as_bytes()
+    offset = content.index(bytes.fromhex('FFD9'))
+    code = content[offset + 2:].decode('utf-8')
+    response = {'hidden_code': code}
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
